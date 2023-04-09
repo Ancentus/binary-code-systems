77f97db0d071f6b8382150804d4fcf6239949e2b
Ancentus <mancentus7@gmail.com>
2023-04-09 08:32:42 +0300

    Print the binary excess-3 code

--- decimal_to_excess3.py
+++ decimal_to_excess3.py
@@ -13,12 +13,16 @@ def decimal_to_excess3(decimal):
         excess3_digit = int(digit) + 3
         # Convert the excess3 digit back to a string and append it to the result
         excess3 += str(excess3_digit)
+
+    # Convert the excess-3 code to binary
+    binary_excess3 = "".join(format(int(c), "04b") for c in excess3)
     
-    return excess3
+    return excess3, binary_excess3
 
 # Take input from the user for the decimal number
 decimal_num = int(input("Enter a decimal number: "))
-excess3_code = decimal_to_excess3(decimal_num)
+excess3_code, binary_excess3_code = decimal_to_excess3(decimal_num)
 
 print("Decimal Number:", decimal_num)
 print("Excess-3 Code:", excess3_code)
+print("Binary Excess-3 Code:", binary_excess3_code)
